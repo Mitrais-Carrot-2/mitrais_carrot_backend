c5c727e17ecb460d56786a4d3ce86bee5cba7944
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-14 13:00:09 +0700

    Add Transfer and History repo Interface

---
+++ src/main/java/com/team/two/mitrais_carrot/repository/TransferRepository.java
@@ -0,0 +1,7 @@
+package com.team.two.mitrais_carrot.repository;
+
+import com.team.two.mitrais_carrot.entity.transfer.TransferEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TransferRepository extends JpaRepository<TransferEntity, Integer> {
+}
