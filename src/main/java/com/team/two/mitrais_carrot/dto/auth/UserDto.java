b3b05316751c00d1911952c53409abde0d1a777e
Ilham Fadhil <ilham.sykes@hotmail.com>
2022-03-15 10:13:35 +0700

    add user able to change password by username

--- src/main/java/com/team/two/mitrais_carrot/dto/auth/UserDto.java
+++ src/main/java/com/team/two/mitrais_carrot/dto/auth/UserDto.java
@@ -2,19 +2,22 @@ package com.team.two.mitrais_carrot.dto.auth;
 
 import java.time.LocalDate;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Getter
 @Setter
 public class UserDto {
+    private boolean isActive;
     private String username;
+    private String firstName;
+    private String lastName;
     private String password;
+    private String address;
+    private LocalDate birthDate;
     private String email;
-//    private String firstName;
-//    private String lastName;
-//    private String address;
-//    private LocalDate birthDate;
-//    private int supervisorId;
-//    private int level;
+    private int supervisorId;
+    private byte[] image;
+    private String jobFamily;
+    private String jobGrade;
+    private String office;
 }
--- src/main/java/com/team/two/mitrais_carrot/service/user/UserService.java
+++ src/main/java/com/team/two/mitrais_carrot/service/user/UserService.java
@@ -11,16 +11,24 @@ import com.team.two.mitrais_carrot.service.basket.BasketService;
 import com.team.two.mitrais_carrot.service.farmer.BarnService;
 import com.team.two.mitrais_carrot.service.merchant.BazaarItemService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.filter.CommonsRequestLoggingFilter;
 
 import java.util.List;
 
+
+
 @Service
 public class UserService {
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
     private BasketRepository basketRepository;
     private BasketService basketService;
+
+    @Autowired
+    PasswordEncoder encoder;
 
     public UserService(UserRepository userRepository) {this.userRepository = userRepository;}
 
@@ -29,7 +37,7 @@ public class UserService {
         return userRepository.save(user);
      }
 
-    public List<UserEntity> getAll(){ return (List<UserEntity>) userRepository.findAll(); }
+    public List<UserEntity> getAll(){ return userRepository.findAll(); }
 
     public UserEntity getById(long id){ return userRepository.findById(id).orElse(null);}
 
@@ -43,6 +51,19 @@ public class UserService {
         basket.setCarrotAmount(newCarrot);
 
         return userRepository.save(user);
+    }
+
+    public UserEntity getByUsername(String username){ return userRepository.findByUsername(username).orElse(null); }
+    //public change password function
+    public Boolean changePassword(String username, String oldPassword, String newPassword){
+        UserEntity user = getByUsername(username);
+        if (encoder.matches(oldPassword, user.getPassword())){
+            user.setPassword(encoder.encode(newPassword));
+            userRepository.save(user);
+            return true;
+        }
+        //return password is false or username is not found
+        return false;
     }
 
 }
